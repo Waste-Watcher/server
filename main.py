ce35cd4a5740dcd1eb36900517d1ee0376a6ed71
Asa Paparo <77820164+AngleSideAngle@users.noreply.github.com>
2021-04-24 13:51:07 -0400

    buggy

--- main.py
+++ main.py
@@ -1,30 +1,53 @@
-from graphene import ObjectType, String, Schema, Int, List
+from graphene import ObjectType, String, Schema, Int, List, Mutation, Field, Boolean
 from flask import Flask, request
 
-class user(ObjectType):
+class User(ObjectType):
     id = String()
     name = String()
     coinCount = Int()
 
 class Query(ObjectType):
-    userList = List(user)
+    userList = List(User)
     
-    def resolve_userList(self, info):
+    def resolve_userList(self, info, id):
         return [
-            user(id="1234",name="asa",coinCount=10),
-            user(id="5678",name="bob",coinCount=12)
+            User(id="1234",name="asa",coinCount=10),
+            User(id="5678",name="bob",coinCount=12)
         ]
 
-schema = Schema(query=Query)
+class newUser(Mutation):
+    class Arguments:
+        print(1)
+        id = String()
+        name = String()
+        coinCount = Int()
 
-answ = schema.execute("""
-{
-    userList {
-        id
-        name
-    }
-}
-""") 
+    ok = Boolean()
+
+    tempUser = Field(User)
+
+    def mutate(self, info, id, name, coinCount):
+        tempUser = User(id = id, name = name, coinCount = coinCount)
+        ok = True
+        print(tempUser)
+        return newUser(tempUser = tempUser, ok=ok)
+
+class Mutations(ObjectType):
+    new_user = newUser.Field()
+
+schema = Schema(query=Query, mutation = Mutations)
+
+answ = schema.execute(
+    """
+        mutation newUser {
+            newUser(id: "12345", name: "test", coinCount: 4){
+                id
+                name
+                coinCount
+            }        
+        }
+    """
+) 
 print(answ)
 '''
 app = Flask(__name__)
