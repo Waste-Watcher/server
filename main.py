c4585f7b68c4d8e7bd7d2b2855a96399a09d3a0e
Asa Paparo <77820164+AngleSideAngle@users.noreply.github.com>
2021-04-24 11:39:39 -0400

    more things in Query

--- main.py
+++ main.py
@@ -2,12 +2,24 @@ from graphene import ObjectType, String, Schema
 from flask import Flask, request
 
 class Query(objectType):
-    pass
+    user = String(
+    id = String()
+    email = String()
+    points = String()
+    )
+
+    def resolve_user(root, info, id):
+        return 
+schema = Schema(query=Query)
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods = ["GET","POST"])
 def main():
-    pass
+    if request.method == "POST":
+        pass
+    if request.method == "GET":
+        pass
+    return 
 
 app.run()
