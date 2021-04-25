41dfdc63ccc1eb5a8a263bbb9dc879bd6de5c5a1
Asa Paparo <77820164+AngleSideAngle@users.noreply.github.com>
2021-04-25 10:30:58 -0400

    that random comma was getting on my nerves

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ schema = graphene.Schema(query = Query, mutation = Mutations)
 app.add_url_rule('/graphql', view_func=GraphQLView.as_view(
     'graphql',
     schema=schema,
-    graphiql=True,
+    graphiql=True
 ))
 
 @app.route('/sendimage', methods=["POST"])
