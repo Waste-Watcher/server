d303bf57b66424ce94cec834de502bd3745e935b
fahim <fahimhussain21@gmail.com>
2021-04-25 10:16:02 -0400

    added item field to query

--- GraphQLObjects.py
+++ GraphQLObjects.py
@@ -13,13 +13,9 @@ class userInfo(graphene.ObjectType):
     earth_coins = graphene.Int()
     items = graphene.List(itemInfo)
 
-class itemInfo(graphene.ObjectType):
-    itemid = graphene.ID()
-    ownderid = graphene.String()
-    item_type = graphene.String()
-
 class Query(graphene.ObjectType):
     User = graphene.Field(userInfo, id=graphene.String())
+    Item = graphene.Field(itemInfo, id=graphene.String())
 
     def resolve_User(root, parent, id):
         user = User.query.filter_by(id = id).first()
